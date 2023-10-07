461e5ed8509380133883ecfd63ad546ba8d52406
okatantow <okatantow@gmail.com>
2023-10-07 16:00:47 +0000

    build home screen with nav bar

---
+++ src/HomeScreen.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './HomeScreen.css';
+import Nav from './Nav';
+
+function HomeScreen() {
+  return (
+    <div className="homeScreen">
+        {/* nav bar */}
+        <Nav/>
+        {/* the banner */}
+    </div>
+  )
+}
+
+export default HomeScreen
