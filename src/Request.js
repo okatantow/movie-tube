6618ebf654e2e5b07e8b5fb95a5de57838deaa47
okatantow <okatantow@gmail.com>
2023-10-07 18:34:22 +0000

    install axios, make an api call to tmdb and display random movie details

---
+++ src/Request.js
@@ -0,0 +1,15 @@
+const API_KEY = "b62e5a496cf3313f8696c0a63054c06d";
+const requests = {
+  fetchTrending: `/trending/all/week?api_key=${API_KEY}&language=en-US`,
+  fetchNetflixOriginals: `/discover/tv?api_key=${API_KEY}&with_networks=213`,
+  fetchTopRated: `/movie/top_rated?api_key=${API_KEY}&language=en-US`,
+  fetchActionMovies: `/discover/movie?api_key=${API_KEY}&with_genres=28`,
+  fetchComedyMovies: `/discover/movie?api_key=${API_KEY}&with_genres=35`,
+  fetchHorrorMovies: `/discover/movie?api_key=${API_KEY}&with_genres=27`,
+  fetchRomanceMovies: `/discover/movie?api_key=${API_KEY}&with_genres=10749`,
+  fetchDocumentaries: `/discover/movie?api_key=${API_KEY}&with_genres=99`,
+
+}
+
+export default requests;
+//"https://api.themoviedb.org/3/trending/all/week?api_key=b62e5a496cf3313f8696c0a63054c06d&language=en-US"
