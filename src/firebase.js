c218b9d0dde516ed7a79e324111f13170e506ec6
okatantow <okatantow@gmail.com>
2023-10-07 14:41:51 +0000

    installed firebase

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCdqLw2DOqY0bLqvc9f3l0szb28CzzrOYM",
+    authDomain: "movie-tube-e7af5.firebaseapp.com",
+    projectId: "movie-tube-e7af5",
+    storageBucket: "movie-tube-e7af5.appspot.com",
+    messagingSenderId: "283042234247",
+    appId: "1:283042234247:web:5976732602c7067ea79d22"
+  };
