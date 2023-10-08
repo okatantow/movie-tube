1cd8ca3f0754990cb3007b4e6348755817ff0bd5
okatantow <okatantow@gmail.com>
2023-10-08 12:36:52 +0000

    setup signup screen and login screen

--- src/App.js
+++ src/App.js
@@ -1,12 +1,28 @@
 import React from 'react';
 import './App.css';
-import HomeScreen from './HomeScreen';
+import HomeScreen from './screens/HomeScreen';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+} from "react-router-dom";
+import LoginScreen from './screens/LoginScreen';
 
 function App() {
+  const user = null;
   return (
     <div className="App">
+      <Router>
+          {!user ? (
+            <LoginScreen/>
+          ):
+          <Routes>
+            <Route exact path="/"  element= {<HomeScreen />}>
+            </Route>
+         </Routes>
+          }
 
-      <HomeScreen />
+      </Router>
     </div>
   );
 }
